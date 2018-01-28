616164f909447e42d00ed7e0bd3e190110fe554d
Howard Payne <paynehd@gmail.com>
2018-01-28 14:52:44 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,5 @@ the 'http://127.0.0.1:8000/' address.
 """
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
