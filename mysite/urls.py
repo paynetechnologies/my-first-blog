95082693ea41d7265f6645aa72b837a83368e33c
Howard Payne <paynehd@gmail.com>
2018-01-28 08:57:46 -0500

    fix Indent error

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # Django 2.01
-from django.contrib import admin
-from django.urls import path
+#from django.contrib import admin
+#from django.urls import path
 #from django.urls import include
 
 # urlpatterns = [
@@ -23,11 +23,11 @@ from django.urls import path
 #     path(r'', include('blog.urls')),
 # ]
 # Django 1.11.9
- from django.conf.urls import include
- from django.conf.urls import url
- from django.contrib import admin
+from django.conf.urls import include
+from django.conf.urls import url
+from django.contrib import admin
 
- urlpatterns = [
-     url(r'^admin/', admin.site.urls),
-     url(r'', include('blog.urls')),
- ]
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+]
